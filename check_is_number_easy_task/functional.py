c13f738df5acfc7dbc5588812982960f33f3cade
TagAgent <arsen.agent47@gmail.com>
2024-05-08 18:23:55 +0600

    LOL

--- check_is_number_easy_task/functional.py
+++ check_is_number_easy_task/functional.py
@@ -2,9 +2,9 @@
 def chek_is_number_easy(number: int):
     counter = 0
     for i in range(2, number):
-        if(number % i == 0):
+        if number % i == 0:
             counter += 1
-    if(counter == 0):
+    if counter == 0:
         print("Число простое")
     else:
         print("Число не простое")
--- palindrom_task/functional.py
+++ palindrom_task/functional.py
@@ -2,7 +2,7 @@
 def chek_is_word_palindrom(word: str):
     """Check is word palindrom"""
     inverse_word = word[::-1]
-    if (inverse_word.lower() == word.lower()):
+    if inverse_word.lower() == word.lower():
         print("Это слово палиндрормом")
     else:
         print("Это слово не палиндром")
